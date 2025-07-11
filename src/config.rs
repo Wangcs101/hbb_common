52826c652c624c294b1275ffa6ae06c00292a34d
Wangcs101 <159117663+Wangcs101@users.noreply.github.com>
2025-07-11 09:16:48 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["115.29.160.221"];
+pub const RS_PUB_KEY: &str = "WMcyNKUmD8xLpLRn9lc437voZdlBzGPNQw48m3w2SHQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
